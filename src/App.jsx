45bdd3840144230d66cf4e834952d1f66a4e540c
AirinNila <airinakhternila>
2025-05-26 11:50:00 +0600

    add form table

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import TableForm from './component/tableForm/tableForm'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <TableForm></TableForm>
+    </>
+  )
+}
+
+export default App
