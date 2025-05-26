77727e4ed4702de5e8586e68fcc5345db5485296
AirinNila <airinakhternila>
2025-05-26 18:30:40 +0600

    use ref in form

--- src/component/refForm/refForm.jsx
+++ src/component/refForm/refForm.jsx
@@ -1,15 +1,22 @@
+import { useEffect, useRef } from "react";
 
 const RefForm = () => {
+    const nameRef = useRef(null)
 
     const handleSubmit = e => {
         e.preventDefault()
+        console.log(nameRef.current.value)
     }
+
+    useEffect(() => {
+        nameRef.current.focus()
+    },[])
     return (
         <div>
              <form  onSubmit={handleSubmit}>
-            <input type="text" name="name" />
+            <input type="text" ref={nameRef} name="name" />
             <br />
-            <input type="email" name="email" />
+            <input type="email" name="email" defaultValue={'nila@gamil.com'} />
             <br />
             <input type="submit" value="Submit" />
             </form>
